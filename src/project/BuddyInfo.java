ce0b59d6961baa2d4b1bdd0e998aae6ed531d1f2
aminqassoud <amin.qassoud@carleton.ca>
2016-09-29 14:58:03 -0400

    init commit

---
+++ src/project/BuddyInfo.java
@@ -0,0 +1,26 @@
+package project;
+
+
+public class BuddyInfo {
+	private int a;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNmae(String hello) {
+		this.name = hello;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+
+
+}
